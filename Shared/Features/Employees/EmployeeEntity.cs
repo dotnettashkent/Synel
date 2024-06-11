caf9deb4a3b124657c154d6fcf6243e20d228d2e
Saeed <dotnettashkent@gmail.com>
2024-06-11 21:19:52 +0500

    [Feat] - Create Entity and View Model

---
+++ Shared/Features/Employees/EmployeeEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Features.Employees
+{
+    public class EmployeeEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string? PayrollNumber { get; set; }
+        public string? Forenames { get; set; }
+        public string? Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Telephone { get; set; }
+        public string? Mobile { get; set; }
+        public string? Address { get; set; }
+        public string? Address2 { get; set; }
+        public string? Postcode { get; set; }
+        public string? EmailHome { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ Shared/Features/Employees/EmployeeView.cs
@@ -0,0 +1,25 @@
+﻿using MemoryPack;
+using Stl.Fusion.Blazor;
+using System.Runtime.Serialization;
+
+namespace Shared.Features.Employees
+{
+    [DataContract, MemoryPackable]
+    [ParameterComparer(typeof(ByValueParameterComparer))]
+    public partial class EmployeeView
+    {
+        [property : DataMember] public long Id { get; set; }
+        [property : DataMember] public string? PayrollNumber { get; set; }
+        [property : DataMember] public string? Forenames { get; set; }
+        [property : DataMember] public string? Surname { get; set; }
+        [property : DataMember] public DateTime DateOfBirth { get; set; }
+        [property : DataMember] public string? Telephone { get; set; }
+        [property : DataMember] public string? Mobile { get; set; }
+        [property : DataMember] public string? Address { get; set; }
+        [property : DataMember] public string? Address2 { get; set; }
+        [property : DataMember] public string? Postcode { get; set; }
+        [property : DataMember] public string? EmailHome { get; set; }
+        [property : DataMember] public DateTime StartDate { get; set; }
+    }
+}
+}
