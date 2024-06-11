f8773eb01e466120edf07a54385b16da0ddd3ceb
Saeed <dotnettashkent@gmail.com>
2024-06-11 22:15:10 +0500

    [Feat] - created interfaces for import and export

---
+++ Shared/Features/Export/IExportService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Infrastructures;
+
+namespace Shared.Features.Export
+{
+    public interface IExportService
+    {
+        Task<string> ExportSubscribersToExcel(TableOptions tableOptions, CancellationToken cancellationToken);
+    }
+}
